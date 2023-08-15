18160ba48ff12e378dc5b52a2c4c0b8901bdf6cf
Devin-Pi <89199086+Devin-Pi@users.noreply.github.com>
2023-08-15 11:48:34 +0800

    repo init

---
+++ evaluate.py
@@ -0,0 +1,20 @@
+# how to conduct evaluation between label and segmentation results?
+import lightning as l
+from PIL import Image
+from torchmetrics.image import
+def compute_mIoU(prediction, png_label):
+    # prediction: 2D array, 0-255
+    # png_label: 2D array, 0-255
+
+    return mIoU
+
+
+
+
+if __name__ == 'main':
+
+    model = model.
+
+    # how to compute the mIoU?
+
+    # how to compute the accuracy?
