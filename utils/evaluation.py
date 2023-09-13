a1e1e1ac289884e3a700e7c23a2e6d804e80b453
Devin Pi <18119447201@163.com>
2023-09-13 21:50:10 +0800

    yolo & seg combination adding

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,5 +1,58 @@
-def save_mask(pred_mask, save_path):
+import torch
+import os
+
+
+def mask_iou(pred, target, eps=1e-7, size_average=True):
+    r"""
+        param: 
+            pred: size [N x H x W]
+            target: size [N x H x W]
+        output:
+            iou: size [1] (size_average=True) or [N] (size_average=False)
+    """
+    assert len(pred.shape) == 3 and pred.shape == target.shape
+
+    N = pred.size(0)
+    num_pixels = pred.size(-1) * pred.size(-2)
+    no_obj_flag = (target.sum(2).sum(1) == 0) # beacuse the pixel value of obj is 1
+
+    temp_pred = torch.sigmoid(pred)
+    pred = (temp_pred > 0.5).int()
+    inter = (pred * target).sum(2).sum(1)
+    union = torch.max(pred, target).sum(2).sum(1)
+
+    inter_no_obj = ((1 - target) * (1 - pred)).sum(2).sum(1)
+    inter[no_obj_flag] = inter_no_obj[no_obj_flag]
+    union[no_obj_flag] = num_pixels
+
+    iou = torch.sum(inter / (union+eps)) / N
+
+    return iou
+
+
+def save_mask(pred_masks, save_base_path, video_name_list):
+    # pred_mask: [bs*5, 1, 224, 224]
+    # print(f"=> {len(video_name_list)} videos in this batch")
+
+    if not os.path.exists(save_base_path):
+        os.makedirs(save_base_path, exist_ok=True)
+
+    pred_masks = pred_masks.squeeze(2)
+    pred_masks = (torch.sigmoid(pred_masks) > 0.5).int()
     
-    
-    
-def mask_iou(pred_mask, save_path):
+    pred_masks = pred_masks.view(-1, 5, pred_masks.shape[-2], pred_masks.shape[-1])
+    pred_masks = pred_masks.cpu().data.numpy().astype(np.uint8)
+    pred_masks *= 255
+    bs = pred_masks.shape[0]
+
+    for idx in range(bs):
+        video_name = video_name_list[idx]
+        mask_save_path = os.path.join(save_base_path, video_name)
+        if not os.path.exists(mask_save_path):
+            os.makedirs(mask_save_path, exist_ok=True)
+        one_video_masks = pred_masks[idx] # [5, 1, 224, 224]
+        for video_id in range(len(one_video_masks)):
+            one_mask = one_video_masks[video_id]
+            output_name = "%s_%d.png"%(video_name, video_id)
+            im = Image.fromarray(one_mask).convert('P')
+            im.save(os.path.join(mask_save_path, output_name), format='PNG')
--- yolo_crack_inference.py
+++ yolo_crack_inference.py
@@ -6,7 +6,6 @@ from PIL import Image
 import numpy as np
 import torch
 
-
 from torch.utils.data import DataLoader
 from dataload.dataload import VOCDataset, make_datapath_list, DataTransform
 from utils.evaluation import save_mask, mask_iou
@@ -14,18 +13,20 @@ from utils.evaluation import save_mask, mask_iou
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
+import torch
+from PIL import Image
+from ultralytics import YOLO
+
 # crack detection
 
 def crack_detection(model, crack_tensor):
-    model = YOLO(".pt") # load a pretrained model (recommended for training)
-    results = model(crack_tensor) # BCHW format with RGB channels float32 (0.0-1.0).
 
-    boxes = results.boxes
-    mask = results.mask
-    keypoints = results.keypoint
-    probs = results.probs
-        
-    crack_detection_result = crack_tensor[:, :, ]    
+    results = model.predict("/unet-semantic/images/processing/flight_2_image_results/img/ppm_538_981337489.jpg",
+                            save=True, conf = 0.5) # BCHW format with RGB channels float32 (0.0-1.0).
+    xywh = results[0].boxes.xywh
+    crack_detection_result = []
+    for i, data in enumerate(xywh): 
+        crack_ = crack_tensor[:, :, ]    
     return crack_detection_result
 
 def crack_segmentation(model, crack_tensor):
@@ -47,35 +48,30 @@ def projection(detection_result, segmenation_result, cfg):
     H = cfg.input_shape[0]
     W = cfg.input_shape[1]
     
-    final_crack_tensor = # 关于如何将mask如贴图一样贴上去，建议参考mask和original img贴在一起的程序
+    final_crack_tensor = 0# 关于如何将mask如贴图一样贴上去，建议参考mask和original img贴在一起的程序
     
     
     return final_crack_tensor
     
-@hydra.main(version_base=None, config_path='/diffusionavs/config', config_name='diffavs_m3_config')    
+@hydra.main(version_base=None, config_path='/unet-semantic/config', config_name='cfg')    
 def main(cfg: DictConfig):
-    
     
     COLOR_MEAN = (0.485, 0.456, 0.406)
     COLOR_STD = (0.229, 0.224, 0.225)
     
-    
-    
-    crack_detection_model = YOLO(cfg.detection_model)
-    crack_segmentation_ = UNet(cfg.segmentation_model.cfg)
-    crack_segmentation_model = crack_segmentation_.load(cfg.segmentation_model.pretrained_path)
-    
-    
+    crack_detection_model = YOLO(cfg.detection_model.pretrained_path)
+    crack_segmentation_ = UNet(cfg.segmentation_model.cfg.c_in, cfg.segmentation_model.cfg.c_out)
+    crack_segmentation_model = crack_segmentation_.load_from_checkpoint(cfg.segmentation_model.pretrained_path)
     train_img_list, train_anno_list, val_img_list, val_anno_list = make_datapath_list(cfg.data_path)
     test_dataset = VOCDataset(val_img_list, val_anno_list,
                               phase='val',
-                               n_classes=cfg.num_classes,
-                               input_shape=cfg.input_shape,
+                               n_classes= 1,
+                               input_shape= 475,
                                transform=DataTransform(input_size=256, color_mean=COLOR_MEAN, color_std=COLOR_STD))
 
     test_dataloader = DataLoader(test_dataset, 
-                                  batch_size=cfg.test_bs,
-                                  num_workers=cfg.num_workers,
+                                  batch_size= 1,
+                                  num_workers= 8,
                                 #   shuffle=True,
                                   pin_memory=True,
                                   drop_last=True)
@@ -85,11 +81,9 @@ def main(cfg: DictConfig):
         img, mask = batch
         crack_detection_result = crack_detection(crack_detection_model, img)
         pred_mask = crack_segmentation(crack_segmentation_model, crack_detection_result)
-        final_mask = projection(crack_detection_result, pred_mask, cfg)
+        # final_mask = projection(crack_detection_result, pred_mask, cfg)
         save_mask(pred_mask)
         mask_iou(pred_mask)
-    
-    
-    
+
 if __name__ == "__main__":
     main()
